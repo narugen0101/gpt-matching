d3ac62e2377c0ff4e0c7a7e850218757534d2584
g.narumi <g.narumi@funrepeat.com>
2023-06-24 20:07:39 +0900

    Ai作成の準備

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,14 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAerG6SgbMROpGotBFnZ7a5C04N0NOjlZU",
-  authDomain: "gpt-matching.firebaseapp.com",
-  projectId: "gpt-matching",
-  storageBucket: "gpt-matching.appspot.com",
-  messagingSenderId: "831976048932",
-  appId: "1:831976048932:web:5b9467eff090cccd3bcbb1",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
